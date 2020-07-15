32eda14ba5b14f061bc2636e18a06dde151e9e86
HassenH1 <hasansaid51@gmail.com>
2020-07-14 17:07:04 -0700

    fixed proxy

--- src/components/contexts/UserContext.js
+++ src/components/contexts/UserContext.js
@@ -1,83 +1,73 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from "react";
 
-export const UserContext = createContext()
+export const UserContext = createContext();
 
 export default function UserContextProvider(props) {
-  
-  const [user, setUser] = useState("")
+  const [user, setUser] = useState("");
 
-  const [wrongValidation, setWrongValidation] = useState("")
+  const [wrongValidation, setWrongValidation] = useState("");
 
   //adding user to backend
   const addUser = async (userInput) => {
     try {
-      const success = await fetch("http://localhost:8000/signup", {
+      const success = await fetch("/signup", {
         method: "POST",
         body: JSON.stringify(userInput),
         headers: {
-          "Content-Type": "application/json"
-        }
-      })
-      const successJson = await success.json()
+          "Content-Type": "application/json",
+        },
+      });
+      const successJson = await success.json();
 
-      localStorage.setItem('user', JSON.stringify(successJson))
+      localStorage.setItem("user", JSON.stringify(successJson));
 
-      getUser(successJson)
-
+      getUser(successJson);
     } catch (err) {
-
-      console.log(err)
-
+      console.log(err);
     }
-  }
+  };
 
   //login user
   const getUser = async (gettingUser) => {
-
     try {
-      const i = await fetch("http://localhost:8000/signin", {
+      const i = await fetch("/signin", {
         method: "POST",
         body: JSON.stringify(gettingUser),
         headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        }
-      })
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
 
-      const iJson = await i.json()
+      const iJson = await i.json();
 
       if (iJson.message === "This is an error!") {
-
-        setWrongValidation("Email or Password doesn't match or exist")
+        setWrongValidation("Email or Password doesn't match or exist");
 
         setTimeout(() => {
-          setWrongValidation("")
-        }, 5000)
-
+          setWrongValidation("");
+        }, 5000);
       } else {
-
-        setUser({ ...iJson })
-        localStorage.setItem('user', JSON.stringify(iJson))
-
+        setUser({ ...iJson });
+        localStorage.setItem("user", JSON.stringify(iJson));
       }
-
     } catch (err) {
-
-      console.log(err)
-
+      console.log(err);
     }
-  }
+  };
 
   //signing out
   const signOutUser = () => {
-    setUser("")
-    localStorage.removeItem('user')
-  }
+    setUser("");
+    localStorage.removeItem("user");
+  };
 
   return (
     //passing values down as "props"
-    <UserContext.Provider value={{ addUser, getUser, signOutUser, user, wrongValidation }}>
+    <UserContext.Provider
+      value={{ addUser, getUser, signOutUser, user, wrongValidation }}
+    >
       {props.children}
     </UserContext.Provider>
-  )
+  );
 }
